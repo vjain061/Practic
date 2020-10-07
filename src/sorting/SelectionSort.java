383acc8a88772b6d74ad79a20846760bfa7633ee
vaibhav <vjain061@gmail.com>
2020-10-07 21:12:09 +0530

    New files

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -12,15 +12,18 @@ public class SelectionSort {
 	}
 	public static int[] selectionSort(int[] arr){
 		int loopCount = 0;
+		int min = 0;
 		for (int i = 0; i < arr.length; i++) {
+			min = i;
 			for (int j = i+1; j < arr.length; j++) {
 				loopCount++;
-				if(arr[i] > arr[j]){
-					int temp = arr[j];
-					arr[j] = arr[i];
-					arr[i] = temp;
+				if(arr[min] > arr[j]){
+					min = j;
 				}
 			}
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
 		}
 		System.out.println("Sorting done in "+loopCount+" iterations");
 		return arr;
